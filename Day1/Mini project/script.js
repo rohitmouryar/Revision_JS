78675b0a8f197bcbf98e4402ac5f43898a5ee19b
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2026-02-02 10:14:29 +0000

    completel mini_project of Day 1

---
+++ Day1/Mini project/script.js
@@ -0,0 +1,31 @@
+// DAY 1 Mini Project
+// ✅ Simple Calculator (Console)
+
+let a = Number(prompt("Enter first number"));
+let b1 = Number(prompt("Enter second number"));
+
+console.log("Sum:", a + b1);
+console.log("Sub:", a - b1);
+console.log("Mul:", a * b1);
+console.log("Div:", a / b1);
+
+// 🔥 EXTRA MINI PROJECTS (DAY 1)
+// ✅ Mini Project 2: Age Validator
+let age = Number(prompt("Enter your age"));
+
+if(age >= 18){
+  console.log("You are eligible");
+}else{
+  console.log("Not eligible");
+}
+
+
+// ✅ Mini Project 3: Simple Login Check
+let username = prompt("Enter username");
+let password = prompt("Enter password");
+
+if(username === "admin" && password === "1234"){
+  console.log("Login Successful");
+}else{
+  console.log("Invalid Credentials");
+}
